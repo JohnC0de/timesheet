d7aa33e177f30f1fb348deeb6113eb67986a172b
João Carlos Magalhães <joaocarlos.m014@gmail.com>
2022-12-26 19:34:11 -0300

    feat: create Trpc User route

---
+++ src/server/trpc/router/user.ts
@@ -0,0 +1,31 @@
+import { TRPCError } from "@trpc/server";
+import { hash } from "argon2";
+import { z } from "zod";
+
+import { router, publicProcedure } from "../trpc";
+
+export const userRouter = router({
+  register: publicProcedure
+    .input(
+      z.object({ name: z.string(), email: z.string(), password: z.string() })
+    )
+    .mutation(async ({ ctx, input }) => {
+      const user = await ctx.prisma.user.findUnique({
+        where: { email: input.email },
+      });
+
+      if (user) {
+        throw new TRPCError({
+          code: "CONFLICT",
+          message: "Email already exists",
+        });
+      }
+      const createdUser = ctx.prisma.user.create({
+        data: {
+          ...input,
+          password: await hash(input.password),
+        },
+      });
+      return createdUser;
+    }),
+});
